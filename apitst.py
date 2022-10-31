16f11352884d39d49e35919e8f1679a50fe7e5d2
linyudoa <50021265+linyudoa@users.noreply.github.com>
2022-10-31 19:54:29 +0800

    added QD-master

--- apitst.py
+++ apitst.py
@@ -1,32 +1,10 @@
+from epynet import Network
 import wdsEnv
-import random
 
-env = wdsEnv.wds(
-        wds_name        = 'anytown_master',
-        speed_increment = .05,
-        episode_len     = 10,
-        pump_group     = [['78', '79']],
-        total_demand_lo = .3,
-        total_demand_hi = 1.1,
-        reset_orig_pump_speeds  = False,
-        reset_orig_demands      = False,
-        seed    = None)
+pathToNetwork = "./water_networks/QDMaster.inp"
 
-# for pipe in env.wds.pipes:
-#     print("pipe index: ", pipe.index)
-#     print("uid: ", pipe.uid)
-#     print("pipe status: ", pipe.status)
-#     print("pipe diameter: ", pipe.diameter)
+pathToNetwork1 = "./water_networks/anytown_master.inp"
 
-prop_code = 2
-mu = 0
-sigma = 10
-print("score", env.get_state_value())
-print("---------------------- trying to add noise ----------------------")
-env.randomize_wds_roughness(mu, sigma)
-print("score", env.get_state_value())
+wds = Network(pathToNetwork1)
 
-# for pump in env.wds.pumps:
-#     print("pump index: ", pump.index)
-#     print("uid: ", pump.uid)
-#     print("pump props: ", pump.properties)
+wds.solve()
